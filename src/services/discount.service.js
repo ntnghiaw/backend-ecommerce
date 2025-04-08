999fb60361d54160dd2953d613a6da7be072477b
Nghia Nguyen <ntnghiaw@gmail.com>
2025-04-08 16:07:41 +0700

    fix discount apply type

--- src/services/discount.service.js
+++ src/services/discount.service.js
@@ -18,11 +18,15 @@ const {
 */
 const { discount } = require('../models/discount.model')
 const { convertToObjectIdMongodb } = require('../utils')
-const { findAllProducts } = require('../models/repositories/product.repo')
+const {
+  findAllProducts,
+  getListProductsByShop,
+  findProductById,
+} = require('../models/repositories/product.repo')
 
 class DiscountService {
-  static async createDiscount({payload, shopId}) {
-    return await createDiscountCode({payload, shopId})
+  static async createDiscount({ payload, shopId }) {
+    return await createDiscountCode({ payload, shopId })
   }
 
   static async updateDiscountCode({}) {}
@@ -103,15 +107,40 @@ class DiscountService {
       discount_users_used,
       discount_value,
       discount_type,
+      discount_products,
+      discount_applies_to
     } = foundDiscount
 
     if (!discount_is_active) throw new NotFoundError('Discount has expired')
-    if ( discount_max_uses <= 0) throw new NotFoundError('Discount not available')
-
+    if (discount_max_uses <= 0) throw new NotFoundError('Discount not available')
+    if (discount_applies_to === 'specific') {
+      products.forEach((product) => {
+        console.log(product)
+        const existProduct = discount_products.find((pro) => {
+          return pro.toString() === product.productId
+        })
+        if (!existProduct) throw new NotFoundError('Discount not available')
+      })
+    }
     let totalOrder = 0
+
+    const foundProducts = await Promise.all(
+      products.map(async (pro) => {
+        const { product_price } = await findProductById({
+          product_id: convertToObjectIdMongodb(pro.productId),
+        })
+        const res = {
+          productId: pro.productId,
+          quantity: pro.quantity,
+          price: product_price,
+        }
+        return res
+      })
+    )
+
     // check total order if min_order_value is required for the discount code
     if (discount_min_order_value > 0) {
-      totalOrder = products.reduce((acc, product) => {
+      totalOrder = foundProducts.reduce((acc, product, idx) => {
         return acc + product.quantity * product.price
       }, 0)
       if (totalOrder < discount_min_order_value)
@@ -130,7 +159,7 @@ class DiscountService {
 
     // check discount type and calculate amount
     const amount =
-      discount_type === 'fixed_amount' ? discount_value : totalOrder * (discount_value / 100)
+      discount_type === 'fixed_amount' ? discount_type : totalOrder * (discount_value / 100)
 
     return {
       totalOrder,
@@ -139,22 +168,20 @@ class DiscountService {
     }
   }
 
-  // level naive =))) 
+  // level naive =)))
   static async deleteDiscountCode({ shopId, code }) {
-
     const deleted = await discount.findOneAndDelete({
       discount_shop: convertToObjectIdMongodb(shopId),
       discount_code: code,
     })
     if (!deleted) throw new NotFoundError('Not found code')
     return deleted
-    
   }
 
-  static async cancelDiscountCode ({ code, userId, shopId}) {
+  static async cancelDiscountCode({ code, userId, shopId }) {
     const foundDiscount = await findOneDiscountCode({
       discount_code: code,
-      discount_shop: shopId
+      discount_shop: shopId,
     })
     if (!foundDiscount) throw new NotFoundError('Discount not found')
 
